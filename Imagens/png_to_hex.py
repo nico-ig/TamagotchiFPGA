e9c4b3adf975ced072fb42bf619fc556da2355d7
schueda <a.schuedamenezes@gmail.com>
2024-11-20 16:16:53 -0300

    Corrige script

--- Imagens/png_to_hex.py
+++ Imagens/png_to_hex.py
@@ -7,8 +7,13 @@ for file in sys.argv[1:]:
     img //= 255
     img = np.concatenate(img)
     img = ''.join(map(str, img))
-    img = hex(int(img, 2))[2:]
-    img = " ".join(img[i:i+2] for i in range(0, len(img), 2))
+
+    hex_f = ""
+    for i in range(0, len(img), 4):
+        hex_f += f"{hex(int(img[i:i+4], 2))[2:]}"
+        if ((i+4) % 8 == 0):
+             hex_f += " "
+    hex_f = hex_f[0:len(hex_f)-1]
 
     with open('hexs/'+file.split('/')[1].split('.')[0]+'.hex', 'w') as f:
-            f.write(f"{img}")
+            f.write(f"{hex_f}")
