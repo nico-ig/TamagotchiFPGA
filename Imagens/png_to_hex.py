bc309b5d61d824aee42ee5b2c398fe8502dc95b4
schueda <a.schuedamenezes@gmail.com>
2024-11-21 22:32:08 -0300

    Coloca os bits invertidos no hex

--- Imagens/png_to_hex.py
+++ Imagens/png_to_hex.py
@@ -8,15 +8,20 @@ for file in sys.argv[1:]:
     img = np.concatenate(img)
     img = ''.join(map(str, img))
 
-    hex_f = ""
+    hex_f = ''
     for i in range(0, len(img), 4):
-        hex_f += f"{hex(int(img[i:i+4], 2))[2:]}"
+        hex_f += f'{hex(int(img[i:i+4], 2))[2:]}'
         if ((i+4) % 8 == 0):
-             hex_f += " "
+             hex_f += ' '
     hex_f = hex_f[0:len(hex_f)-1]
 
-    
+    hex_f = hex_f.split(' ')
 
+    inverted_hex = ''
+    for i in range(0, len(hex_f), 8):
+        inverted_hex += ' '.join(hex_f[i: i+8][::-1]) + ' '
+
+    inverted_hex = inverted_hex[:-1]
 
     with open('hexs/'+file.split('/')[1].split('.')[0]+'.hex', 'w') as f:
-            f.write(f"{hex_f}")
+            f.write(f'{inverted_hex}')
