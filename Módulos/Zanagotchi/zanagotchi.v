3f9e159aa08d3b1d7df4b0885cd536221b2de618
Pedro Amaral <amaral1@ufpr.br>
2024-11-26 20:23:42 -0300

    Instancia controlador de imagens

--- Módulos/Zanagotchi/zanagotchi.v
+++ Módulos/Zanagotchi/zanagotchi.v
@@ -6,6 +6,7 @@ module zanagotchi
     wire[2:0] estado;
     wire[7:0] fome, felicidade, sono;
     wire morreu;
+    wire[1024*8-1:0] imagem;
 
     // Instancia o módulo de controle de estados
     controlador_estados EST
@@ -15,6 +16,14 @@ module zanagotchi
         .b2(b2),
         .morreu(morreu),
         .estado(estado)
+    );
+
+    // Instancia o módulo de controle das imagens para o display
+    controlador_imagens IMG
+    (
+        .clk(clk),
+        .estado(estado),
+        .imagem(imagem)
     );
 
     // Instancia o módulo de controle de atributos
