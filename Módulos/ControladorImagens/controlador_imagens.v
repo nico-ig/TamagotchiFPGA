9dbc41568723a5c6dbaf4cd2c3311fb66a4bb125
schueda <a.schuedamenezes@gmail.com>
2024-11-29 07:16:32 -0300

    Volta os blocos de memória na memória de imagens

--- Módulos/ControladorImagens/controlador_imagens.v
+++ Módulos/ControladorImagens/controlador_imagens.v
@@ -4,7 +4,7 @@ module controlador_imagens
     input wire [3:0] estado,
     output wire [1024*8-1:0] imagem
 );
-    integer i, j;
+    integer i;
 
     // Estados possíveis
     localparam IDLE = 4'b0000, 
@@ -13,37 +13,41 @@ module controlador_imagens
                DANDO_AULA = 4'b0011,
                MORTO = 4'b0100;
 
+    localparam IDLE_SIZE = 1,
+               DORMINDO_SIZE = 1,
+               COMENDO_SIZE = 1,
+               DANDO_AULA_SIZE = 1,
+               MORTO_SIZE = 1;
+
     // Memória para armazenar todas as imagens inicialmente
-    reg[1024*8-1:0] memoria_idle[0:0];
-    reg[1024*8-1:0] memoria_dormindo[0:0];
-    reg[1024*8-1:0] memoria_comendo[0:0];
-    reg[1024*8-1:0] memoria_dando_aula[0:0];
-    reg[1024*8-1:0] memoria_morto[0:0];
-    reg[7:0] tmp[0:1023];
+    reg[7:0] memoria_idle [IDLE_SIZE*1023:0];
+    reg[7:0] memoria_dormindo [DORMINDO_SIZE*1023:0];
+    reg[7:0] memoria_comendo [COMENDO_SIZE*1023:0];
+    reg[7:0] memoria_dando_aula [DANDO_AULA_SIZE*1023:0];
+    reg[7:0] memoria_morto [MORTO_SIZE*1023:0];
 
     // Inicializa a memória
-    initial 
+    initial
     begin
-        $readmemh("./Imagens/hexs/Idle/zanagotchi_idle1.hex", tmp);
-        for (i = 0; i < 1024; i = i + 1)
-            for (j = 0; j < 8; j = j + 1)
-                memoria_idle[0][i*8 + j] = tmp[i][j];
-        $readmemh("./Imagens/hexs/Dormindo/zanagotchi_dormindo1.hex", tmp);
-        for (i = 0; i < 1024; i = i + 1)
-            for (j = 0; j < 8; j = j + 1)
-                memoria_dormindo[0][i*8 + j] = tmp[i][j];
-        $readmemh("./Imagens/hexs/Comendo/zanagotchi_comendo1.hex", tmp);
-        for (i = 0; i < 1024; i = i + 1)
-            for (j = 0; j < 8; j = j + 1)
-                memoria_comendo[0][i*8 + j] = tmp[i][j];
-        $readmemh("./Imagens/hexs/DandoAula/zanagotchi_dando_aula1.hex", tmp);
-        for (i = 0; i < 1024; i = i + 1)
-            for (j = 0; j < 8; j = j + 1)
-                memoria_dando_aula[0][i*8 + j] = tmp[i][j];
-        $readmemh("./Imagens/hexs/Morto/zanagotchi_morto1.hex", tmp);
-        for (i = 0; i < 1024; i = i + 1) 
-            for (j = 0; j < 8; j = j + 1)
-                memoria_morto[0][i*8 + j] = tmp[i][j];
+        for (i = 0; i < IDLE_SIZE; i = i + 1) begin
+            $readmemh($sformatf("../Imagens/hexs/Idle/zanagotchi_idle%0d.hex", i + 1), memoria_idle, i*1024, i*1024 + 1023);
+        end
+
+        for (i = 0; i < DORMINDO_SIZE; i = i + 1) begin
+            $readmemh($sformatf("../Imagens/hexs/Dormindo/zanagotchi_dormindo%0d.hex", i + 1), memoria_dormindo, i*1024, i*1024 + 1023);
+        end
+
+        for (i = 0; i < COMENDO_SIZE; i = i + 1) begin
+            $readmemh($sformatf("../Imagens/hexs/Comendo/zanagotchi_comendo%0d.hex", i + 1), memoria_comendo, i*1024, i*1024 + 1023);
+        end
+
+        for (i = 0; i < DANDO_AULA_SIZE; i = i + 1) begin
+            $readmemh($sformatf("../Imagens/hexs/DandoAula/zanagotchi_dando_aula%0d.hex", i + 1), memoria_dando_aula, i*1024, i*1024 + 1023);
+        end
+
+        for (i = 0; i < MORTO_SIZE; i = i + 1) begin
+            $readmemh($sformatf("../Imagens/hexs/Morto/zanagotchi_morto%0d.hex", i + 1), memoria_morto, i*1024, i*1024 + 1023);
+        end
     end
 
     // Atualização de imagens baseada no estado
